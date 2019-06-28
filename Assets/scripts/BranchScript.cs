996e837309b460c1f87282e0a45baf45e4228683
Arthur Unity <apploidyakutsk@gmail.com>
2019-06-28 15:44:23 +0300

    added branch script

---
+++ Assets/scripts/BranchScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BranchScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] gameObjects;
+    [SerializeField] private string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i] != null)
+            {
+                return;
+            }
+        }
+        SceneManager.LoadScene(nextScene);
+    }
+}
